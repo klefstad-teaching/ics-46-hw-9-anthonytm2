a3fa6483e21d645fd6d613dffcd9a418a779cb89
anthonytm2 <atam14@uci.edu>
2025-03-16 23:40:31 +0000

    should fix formatting

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -42,8 +42,6 @@ void print_path(const vector<int>& v, int total) {
     }
     for (size_t i = 0; i < v.size(); i++) {
         cout << v[i] << " ";
-        if (i == v.size() - 1)
-            cout << endl;
     }
     cout << endl << "Total cost is " << total << endl;
 }
